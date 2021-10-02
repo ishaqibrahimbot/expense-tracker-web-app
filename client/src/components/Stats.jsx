a6a21d721a3c4b7d70f12b934c6f8afd460fd4a6
Ishaq Ibrahim <ishaqibrahimbss@gmail.com>
2021-10-02 10:31:34 +0500

    troubleshoot 1

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -6,6 +6,7 @@ const getFilteredExpenses = expenses => {
     let monthStart = new Date();
     monthStart.setDate(1);
     monthStart.setHours(0, 0, 0);
+    console.log("MONTH START: ", monthStart);
     
     let filteredExpenses = expenses.filter((expense, i) => {
         return new Date(expense.date) >= monthStart;
